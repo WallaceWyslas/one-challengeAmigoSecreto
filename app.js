f96413d07f23e96b81ed74318f2bf9d694ebb7c3
Wallace Wyslas <wallacewyslas@gmail.com>
2025-01-25 18:20:21 -0300

    Em progresso funcao de adicionar amigo na lista e verificar se esta ou n vazio

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 let friends = [];
-document.getElementById = 'amigo';
+let friendsName;
+console.log(friends);
 
-if ()
+// toda vez que aperto esse botão no HTML, ele aciona essa função
+function adicionarAmigo() {
+    if (friendsName != '') {
+        friendsName = document.getElementById('amigo').value;
+        friends.push(friendsName);
+        console.log(friends);
+    } else {
+        alert('Por favor, insira um nome.')
+    }
+}
