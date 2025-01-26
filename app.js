4dd0b0cd1a1c1a7062fbbfa702b6f14178e0c1a3
Wallace Wyslas <wallacewyslas@gmail.com>
2025-01-26 16:11:32 -0300

    limpeza e comentarios do arquivo app.js

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
 let friends = [];
-console.log(friends);
 
 // toda vez que aperto esse botão no HTML, ele aciona essa função
+// essa função verifica se o campo de amigo está vazio
+// se estiver vazio, ela mostra uma mensagem, se n estiver vazio, ela adiciona o amigo na lista, limpa o campo e atualiza a lista de amigos
 function adicionarAmigo() {
     const friendsInput = document.getElementById('amigo').value.trim();
     if (friendsInput !== '') {
         friends.push(friendsInput);
         limparCampo();
         atualizarListaDeAmigos();
-
-        console.log(friends);
     } else {
         alert('Por favor, insira um nome.')
     }
 }
 
+// limpa o campo que digita o nome do amigo
 function limparCampo() {
     document.getElementById('amigo').value = '';
 }
 
+
+// atualiza a lista de amigos
 function atualizarListaDeAmigos() {
   const lista = document.getElementById('listaAmigos');
   lista.innerHTML = "";
@@ -28,4 +30,11 @@ function atualizarListaDeAmigos() {
     elementoLista.textContent = amigo;
     lista.appendChild(elementoLista);
   }
+}
+
+// sortear amigo aleatório
+function sortearAmigo() {
+    const amigoSorteado = friends[Math.floor(Math.random() * friends.length)];
+    const resultado = document.getElementById('resultado');
+    resultado.textContent = amigoSorteado;
 }
