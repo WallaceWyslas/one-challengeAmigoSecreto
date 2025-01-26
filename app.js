45861a1b5fee52880d57bfa4278c5ea587b60ef9
Wallace Wyslas <wallacewyslas@gmail.com>
2025-01-26 14:37:05 -0300

    Cleaning the code

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 let friends = [];
-let friendsName;
 console.log(friends);
 
 // toda vez que aperto esse botão no HTML, ele aciona essa função
 function adicionarAmigo() {
-    const amigoInput = document.getElementById('amigo').value.trim();
-    if (amigoInput !== '') {
-        friends.push(amigoInput);
+    const friendsInput = document.getElementById('amigo').value.trim();
+    if (friendsInput !== '') {
+        friends.push(friendsInput);
         console.log(friends);
     } else {
         alert('Por favor, insira um nome.')
